690c105dd15a2c6627e6411d8e724275d0cceca6
MayoG <dhtnthu9794048@gmail.com>
2022-02-26 13:35:09 +0200

    Fix everything

--- edit_video_utils.py
+++ edit_video_utils.py
@@ -1,23 +1,31 @@
-import os
-import numpy as np
+import dill as pickle
 from moviepy.editor import VideoFileClip, CompositeAudioClip, AudioFileClip
 from moviepy.video.VideoClip import TextClip
 from moviepy.video.compositing.CompositeVideoClip import CompositeVideoClip
 from moviepy.video.compositing.concatenate import concatenate_videoclips
 from moviepy.video.tools.segmenting import findObjects
-import dill as pickle
+import os
+import numpy
 
-colors = {
+colors_to_pickle = {
     (102, 153, 255): "LightBlue1.pkl",
     (218, 112, 214): "orchid.pkl",
-    (255, 0, 0): "red_text.pkl",
+    (255, 0, 0): "red.pkl",
+    (211, 211, 211): "LightGray.pkl",
+    (0, 0, 255): "CadetBlue.pkl",
+}
+
+colors_to_name = {
+    (102, 153, 255): "LightBlue1",
+    (218, 112, 214): "orchid",
+    (255, 0, 0): "red",
     (211, 211, 211): "LightGray",
     (0, 0, 255): "CadetBlue",
 }
 
 
 def load_text(color, texts_path):
-    with open(os.path.join(texts_path, colors[color]), 'rb') as handle:
+    with open(os.path.join(texts_path, colors_to_pickle[color]), 'rb') as handle:
         b = pickle.load(handle)
     return b
 
@@ -33,7 +41,6 @@ def edit_video(video_path, background_music, texts_path, color, new_path=None, d
             audio_background = audio_background.set_end(my_clip.duration - 0.5)
             final_audio = CompositeAudioClip([my_clip.audio, audio_background])
             my_clip = my_clip.set_audio(final_audio)
-            # text_clip = create_intro_text(screensize=my_clip.size, color=color)
             text_clip = load_text(color, texts_path)
             my_clip = CompositeVideoClip([my_clip, text_clip])
             my_clip.write_videofile(new_path, codec="libx264", threads=4, logger=None)
@@ -42,48 +49,56 @@ def edit_video(video_path, background_music, texts_path, color, new_path=None, d
         os.remove(video_path)
 
 
-# def create_intro_text(screensize, color):
-#     text = 'בה"ד קוד'.encode('utf-8')
-#     txtClip = TextClip(text, color=colors.get(color, "black"), font="Guttman-Aharoni-Bold",
-#                        kerning=10, fontsize=100)
-#     cvc = CompositeVideoClip([txtClip.set_pos(('center', 'top'))],
-#                              size=screensize)
-#
-#     # helper function
-#     rotMatrix = lambda a: np.array([[np.cos(a), np.sin(a)],
-#                                     [-np.sin(a), np.cos(a)]])
-#
-#     def cascade(screenpos, i, nletters):
-#         i = nletters - i
-#         v = np.array([0, -1])
-#         d = lambda t: 1 if t < 0 else abs(np.sinc(t) / (1 + t ** 4))
-#         return lambda t: screenpos + v * 400 * d(t - 0.15 * i)
-#
-#     def vortexout(screenpos, i, nletters):
-#         i = nletters - i
-#         d = lambda t: max(0, t)  # damping
-#         a = - np.pi / 2  # angle of the movement
-#         v = rotMatrix(a).dot([-1, 0])
-#         # if i % 2: v[1] = -v[1]
-#         return lambda t: screenpos + 400 * d(t - 0.1 * i) * rotMatrix(-0.2 * d(t) * a).dot(v)
-#
-#     letters = findObjects(cvc, rem_thr=100)
-#
-#     def moveLetters(letters, funcpos):
-#         return [letter.set_pos(funcpos(letter.screenpos, i, len(letters)))
-#                 for i, letter in enumerate(letters)]
-#
-#     clips = [CompositeVideoClip(moveLetters(letters, funcpos),
-#                                 size=screensize).subclip(0, 5)
-#              for funcpos in [cascade, vortexout]]
-#
-#     # WE CONCATENATE EVERYTHING AND WRITE TO A FILE
-#
-#     final_clip = concatenate_videoclips(clips)
-#     pickle.dump(final_clip, open("texts/LightBlue1.pkl", "wb"))
-#     return final_clip
+def create_intro_text(screensize, color, file_name):
+    import numpy
+    text = 'בה"ד קוד'.encode('utf-8')
+    txtClip = TextClip(text, color=colors_to_name.get(color, "black"), font="Guttman-Aharoni-Bold",
+                       kerning=10, fontsize=100)
+    cvc = CompositeVideoClip([txtClip.set_pos(('center', 'top'))],
+                             size=screensize)
+
+    # helper function
+    rotMatrix = lambda a: numpy.array([[numpy.cos(a), numpy.sin(a)],
+                                    [-numpy.sin(a), numpy.cos(a)]])
+
+    def cascade(screenpos, i, nletters):
+        import numpy
+        i = nletters - i
+        v = numpy.array([0, -1])
+        d = lambda t: 1 if t < 0 else abs(numpy.sinc(t) / (1 + t ** 4))
+        return lambda t: screenpos + v * 400 * d(t - 0.15 * i)
+
+    def vortexout(screenpos, i, nletters):
+        i = nletters - i
+        d = lambda t: max(0, t)  # damping
+        a = - numpy.pi / 2  # angle of the movement
+        v = rotMatrix(a).dot([-1, 0])
+        # if i % 2: v[1] = -v[1]
+        return lambda t: screenpos + 400 * d(t - 0.1 * i) * rotMatrix(-0.2 * d(t) * a).dot(v)
+
+    letters = findObjects(cvc, rem_thr=100)
+
+    def moveLetters(letters, funcpos):
+        return [letter.set_pos(funcpos(letter.screenpos, i, len(letters)))
+                for i, letter in enumerate(letters)]
+
+    clips = [CompositeVideoClip(moveLetters(letters, funcpos),
+                                size=screensize).subclip(0, 5)
+             for funcpos in [cascade, vortexout]]
+
+    # WE CONCATENATE EVERYTHING AND WRITE TO A FILE
+
+    final_clip = concatenate_videoclips(clips)
+    with open(file_name, "wb") as file:
+        pickle.dump(final_clip, file)
+    return final_clip
 
 
 if __name__ == '__main__':
-    edit_video(r'example_video.mp4',
-               'utils/music/music_lower.mp3', color=(102, 153, 255), new_path="test123.mp4")
+    # edit_video(r'example_video.mp4',
+    #            'utils/music/music_lower.mp3', texts_path='texts/', color=(102, 153, 255), new_path="test123.mp4")
+    create_intro_text((848, 480), (102, 153, 255), "texts/LightBlue1.pkl")
+    create_intro_text((848, 480), (218, 112, 214), "texts/orchid.pkl")
+    create_intro_text((848, 480), (255, 0, 0), "texts/red.pkl")
+    create_intro_text((848, 480), (211, 211, 211), "texts/LightGray.pkl")
+    create_intro_text((848, 480), (0, 0, 255), "texts/CadetBlue.pkl")
